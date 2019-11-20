690b54e7791a1abedbfff29d69f51c2016440dd1
Giovanna <giovanna.olivelima@gmail.com>
2019-11-20 01:49:03 -0300

    partners

--- src/components/listcard.js
+++ src/components/listcard.js
@@ -3,20 +3,22 @@ import Button from '../components/button.js';
 function ListCard(props) {
     const template = `
     <li data-id='${props.id}' class="${props.class}">
-    <img class = "card-image"  src="${props.img}">
+    <img class = "card-image" src="${props.img}">
     <div class="card-info">
     <p class = "card-name">${props.name}</p>
-    <p class = "card-price">Preço: ${props.price}</p>
-    <p class = "card-classification">Classificação: ${props.classification}</p>
-    <p class = "card-theatre">Teatro: ${props.theater}</p>
-    </div>
-    <div class='buy'>
+    <div class=info>
+    <p class = "card-theatre"><i class="location-icon fas fa-map-marker-alt"></i>   ${props.theater}</p>
+    <img class = "card-classification" src='../Imagens/parental-raiting-icons/${props.classification}.png'>
+    <div class='footer-info'>
+    <p class = "card-price">${props.price}</p>
     ${Button({
         id: props.id,
         class:'btn-buy',
         title: 'Comprar',
         onClick: Buy,
-      })}
+    })}
+    </div>
+    </div>
     </div>
     </li>
     `;  
@@ -25,7 +27,8 @@ function ListCard(props) {
 
 function Buy(e){
     const option = e.target.dataset.id;
-    window.location.href = `${option}`
+    // window.location.href = `${option}`
+    window.open(`${option}`,'_blank')
 }
 
 window.Card = ListCard;
